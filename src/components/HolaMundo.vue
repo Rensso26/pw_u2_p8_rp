e831f9ccb98aa1383f98016dc63e247a1dff95cd
Rensso26 <rnparra@uce.edu.ec>
2025-05-12 18:22:00 -0500

    taller 12

---
+++ src/components/HolaMundo.vue
@@ -0,0 +1,34 @@
+<!--vue+tab estructura principal de un componente-->
+
+<template>
+  <div class="container">
+    <h1 class="titulo">Universidad Central del Evuador</h1>
+    <h2>Facultad de Ingenieria y Ciencias aplicadas</h2>
+    <h3>Carrera de Computaci&#243;n</h3>
+    <h4>Hola Mundo desde Vue</h4>
+    <p>Rensso Parra</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.titulo {
+    color: rebeccapurple;
+}
+
+h2 {
+    background: blanchedalmond;
+}
+
+p {
+    color: greenyellow;
+    background: blueviolet;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 40px;
+}
+</style>
