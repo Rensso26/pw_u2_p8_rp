f471bb8e31df1a8daa97d5a06053c07a38ceb311
Rensso26 <rnparra@uce.edu.ec>
2025-05-15 22:09:21 -0500

    Tarea 3

--- src/components/Estudiante.vue
+++ src/components/Estudiante.vue
@@ -4,11 +4,17 @@
     <input  v-model="nuevoNombre" type="text" id="id_nombre"/>
     <label for="id_apellido">Apellido:</label>
     <input  v-model="nuevoApellido" type="text" id="id_apellido"/>
+    <label for="id_edad">Edad:</label>
+    <input  v-model="nuevaEdad" type="number" id="id_edad"/>
+    <label for="id_genero">Genero:</label>
+    <input  v-model="nuevoGenero" type="text" id="id_genero"/>
+    <label for="id_color">Color Preferido:</label>
+    <input  v-model="nuevoColorPreferido" type="text" id="id_color"/>
     <button v-on:click="agregarEstudiante()">Agregar Estudiante</button>
-    {{nuevoNombre +" "+ nuevoApellido}}
+    {{nuevoNombre +" "+ nuevoApellido + " " + nuevaEdad + " " + nuevoGenero + " " + nuevoColorPreferido}}
     <ul>
-      <li v-for="{ nombre, apellido } in lista" :key="nombre">
-        Nombre: {{ nombre }} - Apellido: {{ apellido }}
+      <li v-for="{ nombre, apellido, edad, genero, colorPreferido } in lista" :key="nombre">
+        Nombre: {{ nombre }} - Apellido: {{ apellido }} - {{edad}} - {{genero}} - {{colorPreferido}}
       </li>
     </ul>
   </div>
@@ -20,14 +26,17 @@ export default {
     return {
         nuevoNombre: "Nuevo",
         nuevoApellido: "Apellido",
+        nuevaEdad:0,
+        nuevoGenero: "Neutro",
+        nuevoColorPreferido: "Celeste",
       lista: [
-        { nombre: "Rensso", apellido: "Parra" },
-        { nombre: "Emil", apellido: "Verkade" },
-        { nombre: "Joel", apellido: "falso" },
-        { nombre: "Kevin", apellido: "Andrade" },
-        { nombre: "Kevin", apellido: "NoAndrade" },
-        { nombre: "Jordy", apellido: "Chamba" },
-        { nombre: "Brayan", apellido: "Sikario" },
+        { nombre: "Rensso", apellido: "Parra", edad: 24, genero: "Masculino", colorPreferido: "rojo"},
+        { nombre: "Emil", apellido: "Verkade", edad: 22, genero: "Masculino", colorPreferido: "verde" },
+        { nombre: "Joel", apellido: "falso", edad: 24, genero: "Masculino", colorPreferido: "azul" },
+        { nombre: "Kevin", apellido: "Andrade", edad: 21, genero: "Masculino", colorPreferido: "negro" },
+        { nombre: "Kevin", apellido: "NoAndrade", edad: 23, genero: "Masculino", colorPreferido: "blanco" },
+        { nombre: "Jordy", apellido: "Chamba", edad: 26, genero: "Masculino", colorPreferido: "naranja" },
+        { nombre: "Brayan", apellido: "Sikario", edad: 27, genero: "Masculino", colorPreferido: "amarillo" },
       ],
     };
   },
@@ -36,6 +45,9 @@ export default {
         const nuevo = {
             nombre: this.nuevoNombre,
             apellido: this.nuevoApellido,
+            edad: this.nuevaEdad,
+            genero: this.nuevoGenero,
+            colorPreferido: this.nuevoColorPreferido
         }
         /*this.lista.unshift(nuevo);*/
         this.lista.push(nuevo);
